c96148a2bc0a13ce302feb3260d96602870f3a9a
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2025-10-18 17:30:44 +0300

    reading from input file more robustly
    reading only the first line in the file now.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 
 input_string: str = ''
 with open('./input.txt') as input_file_content:
-    input_string = input_file_content.read()
+    input_string = input_file_content.readline()
 print(input_string)
 
