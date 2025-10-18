5b17431e2830c1271be6099f88cb56041c9b0233
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2025-10-18 18:51:15 +0300

    frequency count now a tree, made tree construction function
    this is largely untested; Unsure if this is in the correct direction

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+from __future__ import annotations
 from functools import total_ordering
-from typing import List
+from typing import List, Optional
+import copy
+
+
 def get_first_line_from_file(filepath: str) -> str:
     ret: str = ''
     with open('./input.txt') as input_file_content:
         ret = input_file_content.readline()
     return ret
 
+
 @total_ordering  # auto implement bigger than etc etc given less than.
-class FrequencyCount:
+class FrequencyCountNode:
     count: int
-    character: str
-    def __init__(self, character) -> None:
+    character: Optional[str]
+    leftChild: Optional[FrequencyCountNode]  = None
+    rightChild: Optional[FrequencyCountNode]  = None
+    def __init__(self, character = None) -> None:
         self.count = 1
         self.character = character
     
@@ -22,28 +29,47 @@ class FrequencyCount:
         return self.count < other.count
     
     def __str__(self):
-        return f'({self.count}, {self.character})'
+        if self.character:
+            return f'({self.count}, {self.character})'
+        else:
+            return f'{str(self.leftChild)}, {str(self.rightChild)}'
     
+
     def __repr__(self):
         return str(self)
     
 
-
-def construct_sorted_frequencies(string_to_be_counted_from: str) -> List[FrequencyCount]:
-    counts: List[FrequencyCount] = []
+def construct_sorted_frequencies(string_to_be_counted_from: str) -> List[FrequencyCountNode]:
+    counts: List[FrequencyCountNode] = []
     # sort the input string so that I can just add all the characters that are alike at a time.
     # could also use a dict for this, but I prefer this way.
     for character in sorted(string_to_be_counted_from):
         if len(counts) != 0 and counts[-1].character == character:
             counts[-1].count += 1
         else:
-            counts.append(FrequencyCount(character))
+            counts.append(FrequencyCountNode(character))
     return sorted(counts, reverse=True)
 
+def construct_new_level_in_huffman_tree(current_level: List[FrequencyCountNode]) -> List[FrequencyCountNode]:
+    
+    new_level = copy.deepcopy(current_level)
+    smallest_frequency = new_level[-1]
+    second_smallest = new_level[-2]
+    new_node = FrequencyCountNode()
+    new_node.leftChild = smallest_frequency
+    new_node.rightChild = second_smallest
+    new_level.pop()
+    new_level.pop()
+    new_level.append(new_node)
+    return sorted(new_level)
 
 if __name__ == '__main__':
     input_string = get_first_line_from_file('./input.txt')
     sorted_frequencies = construct_sorted_frequencies(input_string)
+    new_level = construct_new_level_in_huffman_tree(sorted_frequencies)
+    while len(new_level) > 1:
+        new_level = construct_new_level_in_huffman_tree(new_level)
     print(input_string)
     print(sorted_frequencies)
+    print(new_level)
 
